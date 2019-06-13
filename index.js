0d9ffe8839e51481c536609ebbc7923ca29f2d74
Malcolm Lynch <milynch1320@gmail.com>
2019-06-12 22:01:26 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+require('dotenv').config()
 const server = require('./api/server.js');
+
+const port = process.env.PORT || 4010
+const greeting = process.env.GREETING
+
 
 server.listen(4000, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
